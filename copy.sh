b8e151b7ea82715fba7bb74b7c466c53db7388c2
dmitri <dmitri.geller@gmail.com>
2017-07-09 21:59:25 +0300

    improving Lunr loading, copy.sh
    Former-commit-id: 4901d54112a38f9bba1e6e805b11d4eacdc2a948

--- copy.sh
+++ copy.sh
@@ -1,7 +1,9 @@
-rm _data/*.json
-cp ../metajob/data-export/*.json _data/
-rm _data/*not.json
-
+rm assets/data/*.json
 rm assets/lunar-index/*.json
+
+cp ../metajob/data-export/*.json assets/data/
 cp ../metajob/data-export/index/*.json assets/lunar-index/
+
+rm assets/data/*not.json
 rm assets/lunar-index/*not.json
+
