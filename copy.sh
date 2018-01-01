c717f756e7158a556da4004b654e377c119a29c1
dmitri <dmitri.geller@gmail.com>
2018-01-01 15:33:04 +0200

    new *.json

--- copy.sh
+++ copy.sh
@@ -11,6 +11,6 @@ rm assets/lunar-index/*.json
 cp ../metajob/data-export/*.json assets/data/
 cp ../metajob/data-export/index/*.json assets/lunar-index/
 
-rm assets/data/*not.json
-rm assets/lunar-index/*not.json
+# rm assets/data/*not.json
+# rm assets/lunar-index/*not.json
 
