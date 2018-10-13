8b6e9959acb6ec3e090872767a6e5fcad87bedf9
dmitri <dmitri.geller@gmail.com>
2018-10-14 00:27:57 +0300

    perf improvements, fonts
    Former-commit-id: 3def3777acc0890439044818f2bbeec23ca807e8

--- _bin/combine.sh
+++ _bin/combine.sh
@@ -12,12 +12,11 @@ cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.
 # handlebars _includes/themes/restart/jobdetails.handlebars >> assets/themes/restart/js/render_jobad_list_filter.js
 
 # write $JSP/jobad-list.js
-cat $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
+cat $JSP/default.js $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
 uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
 
 # write $CSP/default.css
-cleancss -O2 -o $CSP/default.css $CSP/font-open-sans.css $CSP/style.css $CSP/font-awesome.css $CSP/userstyle.css
-
+cleancss -O2 -o $CSP/default.css $CSP/style.css $CSP/userstyle.css
 
 
 
