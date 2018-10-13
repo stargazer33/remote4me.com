7b37ccd2246dac56d99de03ad8905dd38885843c
dmitri <dmitri.geller@gmail.com>
2018-10-13 19:01:51 +0300

    -animate.css
    Former-commit-id: 78312246037719fac713af27f3cb41463f85b595

--- _bin/combine.sh
+++ _bin/combine.sh
@@ -13,10 +13,9 @@ cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.
 cat $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars-v4.0.11.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
 uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
 
-
-# cat $JSP/firebase-app.js $JSP/firebase-auth.js $JSP/firebase-firestore.js $JSP/firebaseui.js > $JSP/firebase.js
-
 # write $CSP/default.css
-cat $CSP/font-open-sans.css $CSP/animate.min.css $CSP/style.min.css $CSP/font-awesome.min.css $CSP/userstyle.css > $CSP/default.css
+cleancss -O2 -o $CSP/default.css $CSP/font-open-sans.css $CSP/style.css $CSP/font-awesome.css $CSP/userstyle.css
+
+
 
 
