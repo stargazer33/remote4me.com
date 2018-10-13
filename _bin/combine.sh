bad7fee00ff822226e6e3101de56666fba467150
dmitri <dmitri.geller@gmail.com>
2018-10-13 14:41:26 +0300

    js files merged into default.js and jobad-list.js

---
+++ _bin/combine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+JSP=assets/themes/restart/js
+
+# write $JSP/default.js
+cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js > $JSP/default.js
+
+# write $JSP/jobad-list.js
+cat $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars-v4.0.11.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
+uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
+
+
+# cat $JSP/firebase-app.js $JSP/firebase-auth.js $JSP/firebase-firestore.js $JSP/firebaseui.js > $JSP/firebase.js
