cb1b36af53708875dde8313725c491efe0802395
dmitri <dmitri.geller@gmail.com>
2018-10-13 16:44:41 +0300

    +default.css
    Former-commit-id: 583d823ed1a894eae3bbfd020c7b78e37281ac3b

--- _bin/combine.sh
+++ _bin/combine.sh
@@ -4,6 +4,7 @@ set -o pipefail
 set -o nounset
 
 JSP=assets/themes/restart/js
+CSP=assets/themes/restart/css
 
 # write $JSP/default.js
 cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js > $JSP/default.js
@@ -14,3 +15,8 @@ uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
 
 
 # cat $JSP/firebase-app.js $JSP/firebase-auth.js $JSP/firebase-firestore.js $JSP/firebaseui.js > $JSP/firebase.js
+
+# write $CSP/default.css
+cat $CSP/font-open-sans.css $CSP/animate.min.css $CSP/style.min.css $CSP/font-awesome.min.css $CSP/userstyle.css > $CSP/default.css
+
+
