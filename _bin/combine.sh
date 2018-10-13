0e9f32513a396bcd1511a101f692f6781d52a51e
dmitri <dmitri.geller@gmail.com>
2018-10-13 22:13:11 +0300

    handlebars full -> handlebars runtime
    Former-commit-id: a7225c6c61c04967d2724d4496fb7f5d066c2091

--- _bin/combine.sh
+++ _bin/combine.sh
@@ -9,8 +9,10 @@ CSP=assets/themes/restart/css
 # write $JSP/default.js
 cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js > $JSP/default.js
 
+# handlebars _includes/themes/restart/jobdetails.handlebars >> assets/themes/restart/js/render_jobad_list_filter.js
+
 # write $JSP/jobad-list.js
-cat $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars-v4.0.11.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
+cat $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
 uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
 
 # write $CSP/default.css
