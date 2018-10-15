976bc6951dc93515feab53df63fa83ab1e49f1de
dmitri <dmitri.geller@gmail.com>
2018-10-15 03:11:27 +0300

    load OpenSans from local CSS
    Former-commit-id: 81c8c30162c49a56fe12a44a5f066ea06a78b687

--- _bin/combine.sh
+++ _bin/combine.sh
@@ -7,13 +7,13 @@ JSP=assets/themes/restart/js
 CSP=assets/themes/restart/css
 
 # write $JSP/default.js
-#cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js $JSP/webfont.js > $JSP/default.js
-uglifyjs --compress unused,dead_code -- $JSP/jquery-1.12.4.min.js $JSP/bootstrap.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js $JSP/webfont.js > $JSP/default.js
+#cat $JSP/jquery-1.12.4.min.js $JSP/bootstrap.min.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js > $JSP/default.js
+uglifyjs --compress unused,dead_code -- $JSP/jquery-1.12.4.min.js $JSP/bootstrap.js $JSP/modernizr.custom.48287.min.js $JSP/jquery.ui.totop.min.js $JSP/easing.min.js $JSP/wow.min.js $JSP/restart_theme.min.js $JSP/collapser.min.js > $JSP/default.js
 
 # write $JSP/jobad-list.js
-#cat $JSP/default.js $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.min.js $JSP/webfont.js > $JSP/jobad-list.js
+#cat $JSP/default.js $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.min.js > $JSP/jobad-list.js
 #uglifyjs --compress -- $JSP/render_jobad_list_filter.js >> $JSP/jobad-list.js
-uglifyjs --compress unused,dead_code -- $JSP/default.js $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.js $JSP/webfont.js $JSP/render_jobad_list_filter.js > $JSP/jobad-list.js
+uglifyjs --compress unused,dead_code -- $JSP/default.js $JSP/bootstrap-table.min.js $JSP/bootstrap-table-en-US.min.js $JSP/handlebars.runtime-v4.0.12.min.js $JSP/lunr.js $JSP/render_jobad_list_filter.js > $JSP/jobad-list.js
 
 # write $CSP/default.css
 cleancss -O2 -o $CSP/default.css $CSP/style.css $CSP/userstyle.css
